9dbfa5e6b6e548210aeafb53d41ccf812aa6df69
Zeeshan Khan <zeeshanxkhan05@gmail.com>
2025-05-19 05:33:17 -0500

    Initial Commit

---
+++ TasbihWatchApp Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TasbihWatchApp Watch App
+//
+//  Created by Zeeshan Khan on 5/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TasbihWatchApp Watch App/TasbihWatchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasbihWatchAppApp.swift
+//  TasbihWatchApp Watch App
+//
+//  Created by Zeeshan Khan on 5/19/25.
+//
+
+import SwiftUI
+
+@main
+struct TasbihWatchApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
